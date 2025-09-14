b9ac4a040d99b6de320d7a175797ecdd14ee7d74
Noir <asitbarandas105@gmail.com>
2025-09-14 20:09:59 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,45 @@
+from email.mime import message
+import discord
+from discord.ext import commands
+import logging
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+bot = commands.Bot(command_prefix='/', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} has connected to Discord!')
+
+@bot.event
+async def on_member_join(member):
+    print(f'{member} has joined the server.')
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+    
+    if "fuck you" in message.content.lower():
+        await message.delete()
+        await message.channel.send(f"{message.author.mention} yo want the hell man")
+
+    await bot.process_commands(message)
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f"Hello {ctx.author.mention}!")
+
+#@bot.tree.command(name="hello_slash", description="Say hello with a slash command")
+#async def hello_slash(interaction: discord.Interaction):
+    await interaction.response.send_message(f"Hello {interaction.user.mention}!")
+
+bot.run(TOKEN, log_handler=handler, log_level=logging.DEBUG)
